7fd237b536cf99e04e006c9b886566e6cdd006e5
cristian <cchura.cpc@gmail.com>
2024-10-22 17:42:28 -0400

    ultimos cambios

--- src/views/admin/producto/Categoria.jsx
+++ src/views/admin/producto/Categoria.jsx
@@ -40,9 +40,9 @@ const Categoria = () => {
         <>
 
 
-            {JSON.stringify(categorias)}
+            {/*JSON.stringify(categorias)*/}
             <button className="bg-blue-500 hover:bg-blue-700 rounded py-2 px-4 text-white" onClick={() => setModalOpen(true)}>Nueva Categoria</button>
-            {JSON.stringify(categoria)}
+            {/*JSON.stringify(categoria)*/}
             <table className="min-w-full bg-white">
                 <thead>
                     <tr>
